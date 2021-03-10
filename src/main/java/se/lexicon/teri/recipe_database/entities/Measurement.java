8caed2eafbc7f5da795ba63fc4466b7660fd0c98
Tamadoc <teri.sandstedt@gmail.com>
2021-03-10 10:32:23 +0100

    Added "label" to Measurement

--- src/main/java/se/lexicon/teri/recipe_database/entities/Measurement.java
+++ src/main/java/se/lexicon/teri/recipe_database/entities/Measurement.java
@@ -1,12 +1,18 @@
 package se.lexicon.teri.recipe_database.entities;
 
 public enum Measurement {
-    TBSP,
-    TSP,
-    KG,
-    G,
-    L,
-    DL,
-    ML,
-    PCS
+    TBSP("Tablespoon"),
+    TSP("Teaspoon"),
+    KG("Kilogram"),
+    G("Gram"),
+    L("Litre"),
+    DL("Decilitre"),
+    ML("Millilitre"),
+    PCS("Pieces");
+
+    public final String label;
+
+    Measurement(String label) {
+        this.label = label;
+    }
 }
